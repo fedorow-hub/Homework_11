7b412f2df3edce56fefe2911be0d4c1396470008
Sergei <s_fedorow@mail.ru>
2022-09-01 15:30:12 +0700

    Проверка работоспособности MainWindowViewModel

--- Homework_11/ViewModels/MainWindowViewModel.cs
+++ Homework_11/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Homework_11.ViewModels.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace Homework_11.ViewModels
 {
-    internal class MainWindowViewModel
+    internal class MainWindowViewModel: ViewModel
     {
+        private string _Title = "База данных клиентов";
+
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get { return _Title; }
+            set => Set(ref _Title, value);     }
     }
 }
