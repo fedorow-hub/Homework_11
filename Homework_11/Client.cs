e8000072db3e6f3c96c4c82a5dc6498dd7c67815
Sergei <s_fedorow@mail.ru>
2022-09-01 12:47:29 +0700

    Добавьте файлы проекта.

---
+++ Homework_11/Client.cs
@@ -0,0 +1,79 @@
+﻿namespace Homework_11;
+
+public class Client //: DependencyObject
+{
+    private string firstname;
+    private string lastname;
+    private string patronymic;
+    private long phoneNumber;
+    private long seriesAndNumberOfPassport;
+
+    public string Firstname { get { return firstname; } set { firstname = value; } }
+
+    public string Lastname { get { return lastname; } set { lastname = value; } }
+
+    public string Patronymic { get { return patronymic; } set { patronymic = value; } }
+
+    public long PhoneNumber { get { return phoneNumber; } set { phoneNumber = value; } }
+
+    public long SeriesAndNumberOfPassport { get { return seriesAndNumberOfPassport; } set { seriesAndNumberOfPassport = value; } }
+
+    public Client(string firstname, string lastname, string patronymic, long phoneNumber, long seriesAndNumberOfPassport)
+    {
+        Firstname = firstname;
+        Lastname = lastname;
+        Patronymic = patronymic;
+        PhoneNumber = phoneNumber;
+        SeriesAndNumberOfPassport = seriesAndNumberOfPassport;
+    }
+
+    public override string ToString()
+    {
+        return $"{firstname} {lastname} {patronymic} {phoneNumber} {seriesAndNumberOfPassport}";
+    }
+
+    //public static readonly DependencyProperty FirstnameProperty;
+    //public static readonly DependencyProperty LastnameProperty;
+    //public static readonly DependencyProperty PatronymicProperty;
+    //public static readonly DependencyProperty PhoneNumberProperty;
+    //public static readonly DependencyProperty SeriesAndNumberOfPassportProperty;
+
+    //static Client()
+    //{
+    //    FirstnameProperty = DependencyProperty.Register("Firstname", typeof(string), typeof(Client));
+    //    LastnameProperty = DependencyProperty.Register("Lastname", typeof(string), typeof(Client));
+    //    PatronymicProperty = DependencyProperty.Register("Patronymic", typeof(string), typeof(Client));
+    //    PhoneNumberProperty = DependencyProperty.Register("PhoneNumber", typeof(long), typeof(Client));
+    //    SeriesAndNumberOfPassportProperty = DependencyProperty.Register("SeriesAndNumberOfPassport", typeof(long), typeof(Client));
+    //}
+    //public string Firstname
+    //{
+    //    get { return (string)GetValue(FirstnameProperty); }
+    //    set { SetValue(FirstnameProperty, value); }
+    //}
+
+    //public string Lastname
+    //{
+    //    get { return (string)GetValue(LastnameProperty); }
+    //    set { SetValue(LastnameProperty, value); }
+    //}
+
+    //public string Patronymic
+    //{
+    //    get { return (string)GetValue(PatronymicProperty); }
+    //    set { SetValue(PatronymicProperty, value); }
+    //}
+
+    //public long PhoneNumber
+    //{
+    //    get { return (long)GetValue(PhoneNumberProperty); }
+    //    set { SetValue(PhoneNumberProperty, value); }
+    //}
+
+    //public long SeriesAndNumberOfPassport
+    //{
+    //    get { return (long)GetValue(SeriesAndNumberOfPassportProperty); }
+    //    set { SetValue(SeriesAndNumberOfPassportProperty, value); }
+    //}    
+}
+
---
+++ Homework_11/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System.Windows.Controls;
+
+namespace Homework_11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Repository repository = new Repository(100);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ListOfClient.ItemsSource = repository.clients;
+            
+        }
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            repository.clients.Remove((Client)ListOfClient.SelectedItem);
+        }
+
+        private void Button_Change_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            PasswordWindow passwordWindow = new PasswordWindow();
+           
+
+            if (passwordWindow.ShowDialog() == true)
+            {
+                
+                    MessageBox.Show("Авторизация пройдена");
+               
+            }
+            else
+            {
+                MessageBox.Show("Авторизация не пройдена");
+            }
+        }
+    }
+    
+}
