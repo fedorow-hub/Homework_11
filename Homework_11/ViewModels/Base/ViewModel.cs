faa9b70c216c2df73a9762fcf705e2dd98a4c6d2
Sergei <s_fedorow@mail.ru>
2022-09-01 14:43:49 +0700

    Базовый класс модели представления

---
+++ Homework_11/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,41 @@
+﻿/*данный класс полезно иметь в шаблонах 
+ для использования в проектах*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string prop = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        /// <summary>
+        /// Метод для обновления значения свойства, для которого определено поле, в котором это свойство хранит свои данные
+        /// (решает проблему кольцевого изменения значений свойств)
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field">ссылка на поле свойства</param>
+        /// <param name="value">новое значение, которое мы хотим установить</param>
+        /// <param name="prop">определяется компилятором самостоятельно</param>
+        /// <returns></returns>
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string prop = null)
+        {
+            if(Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(prop);
+            return true;
+        }
+    }
+}
